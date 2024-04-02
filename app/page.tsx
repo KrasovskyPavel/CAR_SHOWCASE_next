85cff057ddc58b6011cb9a706fdb0f341ec0bd80
Pavel Krasovsky <frostcat2002@gmail.com>
2024-04-02 17:22:30 +0300

    Create: CarCard, CarDetails modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar } from "@/components";
+import { CarCard, Hero, SearchBar } from "@/components";
 import CustomFilter from "@/components/CustomFilter";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -29,7 +29,7 @@ export default async function Home() {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard />
+                <CarCard key={car} car={car} />
               ))}
             </div>
           </section>
--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,8 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
 }
 
 export interface CustomFilterProps {
@@ -15,4 +17,19 @@ export interface CustomFilterProps {
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
